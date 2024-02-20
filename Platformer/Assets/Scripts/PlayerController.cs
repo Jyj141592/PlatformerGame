961cb94b508ffa3789b39b9482f4830cb65b493b
Jyj141592 <140074412+Jyj141592@users.noreply.github.com>
2024-02-21 07:32:34 +0900

    AddAssets

--- Platformer/Assets/Scripts/PlayerController.cs
+++ Platformer/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Burst.Intrinsics;
@@ -6,37 +7,61 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    // Components
     public Rigidbody2D rigid;
     public Animator animator;
-    public BoxCollider2D col;
+    public BoxCollider2D bCol;
     public SpriteRenderer sprite;
-    public int direction = -1;
 
+    // States
+    public int direction = -1;
     public float speed = 5.0f;
     public float power = 8.0f;
+    public float holdTime = 1.5f;
     private float pressTime = 0.0f;
 
+    // Conditions
     public bool isAir{
-        get => Physics2D.OverlapCircle(transform.position, 0.05f, groundLM) == null;
+        get => Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y - 1), 0.05f, groundLM) == null;
     }
+    private bool isCharge = false;
 
+    // Layers
     int groundLayer;
     int groundLM;
 
+    // Hashes
+    int isAir_hash;
+    int charge_hash;
+    int move_hash;
+    int speed_hash;
+    int hit_hash;
+
     void Start()
     {
+        // Initialize Components
         rigid = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        col = GetComponent<BoxCollider2D>();
+        bCol = GetComponent<BoxCollider2D>();
         sprite = GetComponent<SpriteRenderer>();
+        // Initialize Layers
         groundLayer = LayerMask.NameToLayer("Ground");
         groundLM = 1 << groundLayer;
+        // Initialize Hashes
+        isAir_hash = Animator.StringToHash("IsAir");
+        charge_hash = Animator.StringToHash("Charge");
+        move_hash = Animator.StringToHash("Move");
+        speed_hash = Animator.StringToHash("Speed");
+        hit_hash = Animator.StringToHash("Hit");
+        Time.timeScale = 0.1f;
     }
 
     void Update()
     {
         if(!isAir){
+            animator.SetBool(isAir_hash, false);
             if(Input.GetKey(KeyCode.LeftArrow)){
+                animator.SetBool(move_hash, true);
                 if(direction == 1){
                     direction = -1;
                     sprite.flipX = false;
@@ -44,6 +69,7 @@ public class PlayerController : MonoBehaviour
                 rigid.velocity = new Vector2(speed * direction, rigid.velocity.y);
             }
             else if(Input.GetKey(KeyCode.RightArrow)){
+                animator.SetBool(move_hash, true);
                 if(direction == -1){
                     direction = 1;
                     sprite.flipX = true;
@@ -52,19 +78,52 @@ public class PlayerController : MonoBehaviour
             }
             else if(Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow)){
                 rigid.velocity = Vector2.zero;
+                animator.SetBool(move_hash, false);
             }
-            else if(Input.GetKeyDown(KeyCode.Space)){
-                rigid.velocity = Vector2.zero;
-                pressTime = Time.time;
+            if(Input.GetKey(KeyCode.Space)){
+                if(Input.GetKeyDown(KeyCode.Space)){
+                    rigid.velocity = Vector2.zero;
+                    pressTime = Time.time;
+                    isCharge = true;
+                    animator.SetBool(charge_hash, true);
+                }
+                if(Time.time - pressTime > holdTime){
+                    Jump();
+                }
             }
-            else if(Input.GetKeyUp(KeyCode.Space)){
-                //Debug.Log((Time.time - pressTime).ToString() + " Jump!");
-                float time = Time.time - pressTime;
-                if(time > 2) time = 2.0f;
-                float angle = Mathf.PI * time / 4;
-                Vector2 jumpDir = new Vector2(Mathf.Cos(angle) * power * direction, Mathf.Sin(angle) * power);
-                rigid.velocity = jumpDir;
+            else if(Input.GetKeyUp(KeyCode.Space) && isCharge){
+                Jump();
             }
+            
+        }
+        else{
+            animator.SetBool(isAir_hash, true);
+            animator.SetFloat(speed_hash, rigid.velocity.sqrMagnitude);
+            Vector2 v = rigid.velocity;
+            if(v == Vector2.zero) return;
+            v.Normalize();
+            float angle = Mathf.Acos(v.y);
+            angle = angle * 180 * direction * -1 / Mathf.PI;
+            //transform.rotation = Quaternion.Euler(0,0,angle);
+            rigid.rotation = angle;
+        }
+    }
+
+    private void Jump(){
+        isCharge = false;
+        animator.SetBool(charge_hash, false);
+        float time = Time.time - pressTime;
+        if(time > holdTime) time = holdTime;
+        float angle = Mathf.PI * time / (2 * holdTime);
+        Vector2 jumpDir = new Vector2(Mathf.Cos(angle) * power * direction, Mathf.Sin(angle) * power);
+        rigid.velocity = jumpDir;
+
+        animator.SetFloat(speed_hash, rigid.velocity.sqrMagnitude);
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.gameObject.layer == groundLayer){
+            animator.SetTrigger(hit_hash);
         }
     }
 }
